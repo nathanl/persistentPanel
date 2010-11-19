aeeb367a5728eab2da134fe0b29afa81d5384a30
Nathan Long <nathan.long@tma1.com>
2010-11-19 17:01:27 -0500

    Added example page.

--- jquery.persistentpanel.js
+++ jquery.persistentpanel.js
@@ -1,107 +1,94 @@
 // ---------------------------------------------
-// persistentPanel namespace
-window.persistentPanel = {};
-
-// ---------------------------------------------
 // persistentPanel sidebar
 // ---------------------------------------------
 
-$.fn.persistentPanel = function(userOptions) {
-
-	// -- Define default options and override with user-specified --
-	//
-	// Expose this for external modification
-	$.fn.persistentPanel.defaults = {
-		openDirection: 'down',
-		// Find toggler element inside self
-		toggler: $('.toggler',$(this)),
-		togglerClassOpen: 'open',
-		togglerClassClosed: 'closed',
-		animateFunction: 'TODO'
-	};
-	// Shorter name for internal use
-	var defaults = $.fn.persisentPanel.defaults;
-
-	// Merging into an empty hash doesn't modify defaults
-	var settings = $.extend({}, defaults, userOptions);
-
-	// Some default unicode arrows to use in toggler
-	var toggleContentDefaults = {
-		'up':			{'closed': '&#25BC', 'open': '&#25B2'},
-		'down':		{'closed': '&#25B2', 'open': '&#25BC'},
-		'left':		{'closed': '&#25C0', 'open': '&#25B6'},
-		'right':	{'closed': '&#25B6', 'open': '&#25C0'},
-	};
-	/*
-	var toggleContentDefaults = {
-		'up':			{'closed': 'V', 'open': '^'},
-		'down':		{'closed': '^', 'open': 'V'},
-		'left':		{'closed': '<', 'open': '>'},
-		'right':	{'closed': '>', 'open': '<'},
-	};
-	*/
-
-	// If user didn't specify toggle Contents or disable them, use default ones
-	if (!settings['togglerContentsOpen'] && !settings['noTogglerContents']) {
-		settings.toggleContentOpen = toggleContentDefaults[settings.openDirection['open']];
-	}
-	if (!settings['togglerContentsClosed'] && !settings['noTogglerContents']) {
-		settings.toggleContentClosed = toggleContentDefaults[settings.openDirection['closed']];
-	}
-};
-
 /*
+* $('#panel').persistentPanel();
+*
+* $('#panel').persistentPanel({toggler: $('.togglerThing',this)});
+*
+*
+* */
+(function( $ ) {
+	$.fn.persistentPanel = function(userOptions) {
+		panel = this;
+		// -- Define default options and override with user-specified --
+		//
+		// Short name for internal use & expose for external modification
+		var defaults = $.fn.persistentPanel.defaults = {
+			openDirection: 'down',
+			// Find toggler element inside self
+			toggler: '#toggler',
+			togglerClassOpen: 'open',
+			togglerClassClosed: 'closed',
+			noTogglerContents: false,
+			// Functions
+			getStatusCookie: function() {return $.cookie('persistentPanelOpen');},
+			setCookieOpen:  function() {$.cookie('persistentPanelOpen', 'true', { expires: 30, path: '/'})},
+			setCookieClosed: function() {$.cookie('persistentPanelOpen', 'false', { expires: 30, path: '/'})},
+			openFunction: function(speed) {panel.show(speed); },
+			closeFunction: function(speed) {panel.hide(speed);},
+			speed: 500
+		};
 
-persistentPanel.sideBar = {
+		// Merging into an empty hash doesn't modify defaults
+		var settings = $.extend({}, defaults, userOptions);
 
-	element: $('#sidebar'),
-	button: $('.expander a',this.element),
-	isOpen: false,
+		// Some default unicode arrows to use in toggler
+		var toggleContentDefaults = $.fn.persistentPanel.toggleContentDefaults = {
+			//								    ▼    						  ▲	
+			'up':			{'closed': '&#25BC', 'open': '&#25B2'},
+			//								    ▲    						  ▼	
+			'down':		{'closed': '&#25B2', 'open': '&#25BC'},
+			//								    ◀                 ▶ 
+			'left':		{'closed': '&#25C0', 'open': '&#25B6'},
+			//								    ▶                 ◀ 
+			'right':	{'closed': '&#25B6', 'open': '&#25C0'},
+		};
 
-	initialize: function(){
-		var sideBarLeftOpen = $.cookie('sideBarIsOpen');
-
-		// If the cookie says it's closed or there's no cookie
-		if (sideBarLeftOpen != 'true') {
-			this.close(0);
-		} else {
-			// Running this makes sure the button looks correct
-			this.open(0);
+		// If user didn't specify toggle contents or disable them, use default ones
+		if (!settings['togglerContentsOpen'] && !settings['noTogglerContents']) {
+			settings.toggleContentOpen = toggleContentDefaults[settings.openDirection['open']];
+		}
+		if (!settings['togglerContentsClosed'] && !settings['noTogglerContents']) {
+			settings.toggleContentClosed = toggleContentDefaults[settings.openDirection['closed']];
 		}
 
-	},
+		var isOpen = true;
 
-	close: function(ms) {
-					 this.element.animate({left:'-20%'}, {duration: ms});
-					 $('#main').animate({left:'-20%'}, {duration: ms});
+		var open = function() {
+			// TODO: do this like settings.openFunction();
+			settings['openFunction'].call();
+			settings['setCookieOpen'].call();
+			isOpen = true;
+			// TODO: Set toggler contents
+		}
 
-					 // Use right-arrow unicode character
-					 this.element.find('.expander a').removeClass('close').addClass('open').html('&#9656;');
+		var close = function() {
+			settings['closeFunction'].call();
+			settings['setCookieClosed'].call();
+			isOpen = false;
+			// TODO: Set toggler contents
+		};
 
-					 // Save state in object and in a cookie (requires jquery.cookie.js)
-					 this.isOpen = false;
-					 $.cookie('sideBarIsOpen', 'false', { expires: 30, path: '/'});
-				 },
+		$(settings['toggler']).click(function(){
+			cookieSetOpen = settings['getStatusCookie'].call();
+			console.log(cookieSetOpen);
+			if (isOpen) {
+				close();
+			} else {
+				open();
+			}
+		});
 
-	open: function(ms) {
-					this.element.animate({left:'0%'}, {duration: ms});
-					$('#main').animate({left:'0%'}, {duration: ms});
+		// Intialize open/closed state on page load
+		// TODO: FIX THIS!!
+		cookieSetOpen = settings['getStatusCookie'].call();
+		if (!cookieSetOpen) {
+			close();
+		}
 
-					// Use left-arrow unicode character
-					this.element.find('.expander a').removeClass('open').addClass('close').html('&#9666;');
-
-					// Save state in object and in a cookie (requires jquery.cookie.js)
-					this.isOpen = true;
-					$.cookie('sideBarIsOpen', 'true', { expires: 30, path: '/'});
-				},
-
-	toggle: function(ms) {
-						this.isOpen ? this.close(ms) : this.open(ms);
-					}
-};
-
-persistentPanel.sideBar.button.click(function(e){
-	persistentPanel.sideBar.toggle(300);
-	e.preventDefault();
-});
-*/
+		// Maintain chainability
+		return this;
+	};
+})( jQuery );
